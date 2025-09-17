395656a20269ed89ff148d19b3159b887cd2aea1
Hamza <158600603+TheKatanaBearer@users.noreply.github.com>
2025-09-17 13:35:53 +0500

    completed..just comments left

---
+++ To do app/script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const list_container = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must right something!")
+
+    }
+
+    else{
+        let li=document.createElement("li");
+        li.innerHTML= inputBox.value;
+        list_container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value="";
+}
+
+
+list_container.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", list_container.innerHTML);
+}
+
+function showTask(){
+    list_container.innerHTML = localStorage.getItem("data");
+    
+}
+
+showTask();
