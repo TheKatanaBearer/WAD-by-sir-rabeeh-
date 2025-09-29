52fba84d25cefd6be1946ec1b45edb21ae13eb93
Hamza <158600603+TheKatanaBearer@users.noreply.github.com>
2025-09-29 11:39:14 +0500

    just commiting cz i gave up fixing the del issue

--- Youtube lookalike/script.js
+++ Youtube lookalike/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
@@ -11,3 +12,18 @@ menuIcon.onclick=function(){
 
 
 
+=======
+
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container =document.querySelector(".container");
+
+menuIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+   container.classList.toggle("large-container"); 
+}
+
+
+
+
+>>>>>>> f51d1a5 (removed the lab)
