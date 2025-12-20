2f88913f65eb610071c24eacd68bf013450f9430
Hamza <158600603+TheKatanaBearer@users.noreply.github.com>
2025-12-20 09:03:20 +0500

    made a correction to index

--- JS/deployment/api/index.js
+++ JS/deployment/api/index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') { 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
