9627c79112805c0586a8715892118e396c9963e7
Hamza <158600603+TheKatanaBearer@users.noreply.github.com>
2025-12-20 10:11:09 +0500

    made public as root

--- JS/deployment/api/index.js
+++ JS/deployment/api/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Change this:
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const connectToDatabase = async ()=>{
     try {
